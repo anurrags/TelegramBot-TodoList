d083ff0b35aa6f95afa76a9f64bb1a9ba341e3b6
Anurag Sharma <iamanurag0501@gmail.com>
2022-12-26 23:37:30 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const TelegramBot = require("node-telegram-bot-api");
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+const uri = process.env.MONGOD;
+
+const todoSchema = new mongoose.Schema({
+  text: String,
+  chatId: String,
+});
+
+const Todo = mongoose.model("Todo", todoSchema);
+
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error(err));
+
+const bot = new TelegramBot("5596144059:AAHKO9v7FKWYz9DD_upVTGo39rHMOQOrD4c", {
+  polling: true,
+});
+
+async function handleCommand(command, chatId, text) {
+  if (command === "/add") {
+    // Add a new todo item
+    const todo = new Todo({ text: text, chatId: chatId });
+    await todo.save();
+    bot.sendMessage(chatId, "Todo item added");
+  } else if (command === "/list") {
+    // Get the list of todo items for the chat
+    const cursor = Todo.find({ chatId: chatId }).cursor();
+    const todos = [];
+    await cursor.forEach((doc) => {
+      todos.push(doc);
+    });
+    bot.sendMessage(
+      chatId,
+      `Todo list:\n${todos.map((t) => t.text).join("\n")}`
+    );
+  } else if (command === "/remove") {
+    // Remove a todo item
+    await Todo.deleteOne({ text: text, chatId: chatId });
+    bot.sendMessage(chatId, "Todo item removed");
+  }
+}
+
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+  const message = msg.text;
+
+  // Check if the message is a command
+  if (message.startsWith("/")) {
+    // Split the message into the command and the text of the todo item
+    const parts = message.split(" ");
+    const command = parts[0];
+    const text = parts.slice(1).join(" ");
+    // Handle the command
+    handleCommand(command, chatId, text);
+  }
+});
